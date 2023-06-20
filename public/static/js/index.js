ac43042d16e443536cc0211c2cd9a62b9e945900
Reginald Robinson <ddrbnsn@gmail.com>
2023-06-20 13:02:20 -0700

    moved js out of html

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,5 +1,14 @@
 const socket = io();
+let currColor;
+
+const nameBox = document.getElementById("name");
+const giddyUpAudio = new Audio("./assets/audio/giddyUp.mp3");
 
 const newHorseName = (name) => {
-  socket.emit("newHorseName", name);
+  socket.emit("newHorseName", { name: name, color: currColor || 180 });
 };
+function changeColor() {
+  nameBox.addEventListener("click", () => giddyUpAudio.play());
+  currColor = document.getElementById("colorChooser").value;
+  document.getElementById("horse").style.filter = `hue-rotate(${currColor}deg)`;
+}
