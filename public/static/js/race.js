a47ae41408afd32bca03313dea9ca3e1c133a255
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-04 12:44:18 -0700

    minor tweaks

--- public/static/js/race.js
+++ public/static/js/race.js
@@ -74,32 +74,25 @@ const main = (p) => {
     socket.on("over", (winner) => {
       console.log(winner);
       p.noLoop();
-      // window.location.href = "/end";
+      window.location.href = "/end";
     });
-
-    // socket.emit("raceOrder", (order) => {
-    // p.order = order;
-    // });
 
     let cnv = p.createCanvas(p.clientWidth, p.clientHeight);
     cnv.parent("raceCanvas");
   };
 
-
   p.dyeHorses = (data) => {
     Object.keys(data)
       .reverse()
       .forEach((horse) => {
-        const pic1 = p.horseImg.get();
-        const pic2 = p.horseImg2.get();
-        const pic3 = p.horseImg3.get();
+        const pics = [p.horseImg.get(), p.horseImg2.get(), p.horseImg3.get()];
         data[horse].color.a = 5; // hack, put this in server or soemthing
-        p.addFilter(pic1, data[horse].color);
-        p.addFilter(pic2, data[horse].color);
-        p.addFilter(pic3, data[horse].color);
+        pics.forEach((pic) => {
+          p.addFilter(pic, data[horse].color);
+        });
         p.horses.set(data[horse].position.y, {
           name: horse,
-          images: [pic1, pic2, pic3],
+          images: pics,
           offset: Math.floor(p.random(10)),
           frame: 0,
         });
@@ -110,19 +103,17 @@ const main = (p) => {
     img.loadPixels();
     for (let i = 0; i < img.width; i++) {
       for (let j = 0; j < img.height; j++) {
-        let index = 4 * (j * img.width + i);
-        let alpha = img.pixels[index + 3];
-        let bright = (r + g + b) / 3;
+        const index = 4 * (j * img.width + i);
+        const alpha = img.pixels[index + 3];
+        const bright = (r + g + b) / 3;
 
         if (alpha !== 0) {
-          // if pixel is not transparent
           img.pixels[index] += (r - bright) * (bright / 255); // Red
           img.pixels[index + 1] += (g - bright) * (bright / 255); // Green    ///get pixel, add color, subract brightness, increase contrast
           img.pixels[index + 2] += (b - bright) * (bright / 255); // Blue
           img.pixels[index + 3] += a; // Blue
         }
       }
-
       img.updatePixels();
     }
   };
@@ -131,71 +122,17 @@ const main = (p) => {
     const horseIndex = horse.position.y
     const horseData = p.horses.get(horseIndex);
     const x = horse.position.x,
-      y = p.clientHeight - 100 - horse.position.y;
-    // animationWindow = 25 * (1 - horse.speed / 10),
-    // stepInCycle = Math.floor(
-    // ((p.frameCount + horseData.offset) % animationWindow) /
-    // (animationWindow / horseData.images.length)
-    // );
-    // console.log("hs,aw,sic: ",horse.speed,animationWindow,stepInCycle)
+      y = p.clientHeight - 110 - horse.position.y;
     const stepInCycle = Math.floor((p.frameCount % 20) / 7);
     const pic = horseData.images[stepInCycle];
     p.text(horse.name, x, y);
-    console.log("showhorse pic", pic, stepInCycle)
     p.image(pic, x, y);
   };
 
-  p.keyPressed = () => {
-    if (p.key === " ") {
-      console.log("Spacebaar");
-      socket.emit("clients", {}, (data) => {
-        console.log("a thing");
-        console.log(data);
-      });
-    }
-  };
-
   p.draw = () => {
-    // if (p.order == -1) {
     socket.emit("frame");
-    // }
   };
 };
 
 const my = new p5(main);
 window.my = my;
-
-
-
-
-
-
-
-
-
-
-
-/**
- * 
- * 000011112222000001111122222
- * |||||||||||||||||||||||||||||||||||||||||||||||
- * 0123400123456789
- * 001122330112233
- * 0246024680
- * frames
- * each frame we recieve a speed
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { join } from "path";
 import { Server } from "socket.io";
 import session from "express-session";
 import sharedsession from "express-socket.io-session";
-import Horse from "./Horse.js";
+// import Horse from "./Horse.js";
 import router from "./routes.js";
 import {
   handleFinale,
@@ -26,7 +26,7 @@ const sessionMiddleware = session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000, // sets the cookie to expire in 1 day
+    maxAge: 60 * 60 * 1000, // sets the cookie to expire in 1 hour
     secure: false, // true for https
     httpOnly: false, // if true prevents client side JS from reading the cookie
     sameSite: "lax", // protection against cross site request forgery attacks
@@ -44,7 +44,6 @@ const clearEmptyClients = ()=>{
     for (let client in clients) {
       if (Object.keys(clients[client]).length === 0) {
         delete clients[client];
-        console.log("Eliminated")
       }
     }
 }
@@ -58,49 +57,48 @@ io.use(sharedsession(sessionMiddleware, { autoSave: true }));
 
 io.on("connection", (socket) => {
   const clientKey = socket.handshake.session.id;
-  clients[clientKey] = clients[clientKey] || {};
+  clients[clientKey] = clients[clientKey] || {}; //FIXME: add supprt for arrays and stuff this need to be more thougtul
 
   const user = clients[clientKey];
-  socket.emit("test", "tesing animal")
- let clientNames = Object.values(clients)
-  .filter(client => client.horse && client.horse.name)
-  .map(client => client.horse.name);
 
-  console.log("User Assigned", user, "Everyone", clientNames);
+  let clientNames = Object.values(clients)
+    .filter((client) => client.horse && client.horse.name)
+    .map((client) => client.horse.name);
+
+  console.log("User Assigned", user.horse ? user.horse.name:user, "Everyone", clientNames);
+
   socket.on("clients", handleClients(socket, clients));
- 
+
   socket.on("newHorse", (socket) => {
     handleNewHorse(socket, clientKey, clients);
   });
 
   socket.on("askForHorse", (response) => {
     if (Object.keys(user).length === 0) return;
-    handleAskForHorse(response, {user:user,clients:clients,io:io});
+    handleAskForHorse(response, { user: user, clients: clients, io: io });
   });
 
-  socket.on("joinLobby", ()=>{
-    console.log("attmept at lobbyjoin")
+  socket.on("joinLobby", () => {
     if (Object.keys(user).length === 0) return;
-    handleLobbyJoin(clients,io)
-  })
+    handleLobbyJoin(clients, io);
+  });
 
   socket.on("ready", (request) => {
-    clearEmptyClients()
+    clearEmptyClients();
     if (Object.keys(user).length === 0) return;
     handleNewStats(request, user);
     handleReady(user, clientKey, clients, io);
-
   });
- 
-   socket.on("frame", () => {
+
+  socket.on("frame", () => {
     if (Object.keys(user).length === 0) return;
     handleFrame(clients, io);
   });
 
-  socket.on('getStandings',()=>{
+  socket.on("getStandings", () => {
     if (Object.keys(user).length === 0) return;
-    handleFinale(clientKey,clients,socket)
-  })
+    handleFinale(clientKey, clients, socket);
+  });
 
   socket.on("disconnect", () => {
     handleDisconnect(clientKey);
@@ -116,5 +114,4 @@ export const isAllClientsReady = (clients) => {
     }
   }
   return counter === 0;
-  // return Object.values(clients).every((client) => client.ready === true);
 };
